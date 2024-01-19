f83903994de5b408abe9f12e51bfef37527401d0
Nahima-ayni <nahimamadey1@gmail.com>
2024-01-19 17:45:26 +0300

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - function to implement bubble sort
+ * @array - array
+ * @size - number of elements in the array
+ * Return: void, nothing to return
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}	
+		}
+	}
+}
+
+/**
+ * swap - function to swap elements
+ * @a: paramaters
+ * @b: paramaters
+ *
+ */
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
